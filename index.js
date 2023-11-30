893a463dfb56a766b4c80d154d6fc09a8a871316
huyquangphamname <68848760+huyquangphamname@users.noreply.github.com>
2023-11-30 21:41:02 +0800

    Via folder provided by Scrimba code
    File include, Image, node modules, js files, html and complete code on purpose of education and test offline

---
+++ index.js
@@ -0,0 +1,55 @@
+import { process } from '/env'
+import { Configuration, OpenAIApi } from 'openai'
+
+const setupTextarea = document.getElementById('setup-textarea')
+const setupInputContainer = document.getElementById('setup-input-container')
+const movieBossText = document.getElementById('movie-boss-text')
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(configuration)
+
+document.getElementById("send-btn").addEventListener("click", async () => {
+  // if (setupTextarea.value) {
+  setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
+  movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
+  // }
+  await fetchBotReply()
+})
+
+async function fetchBotReply() {
+  const promptText = setupTextarea.value || 'Sound enthusiastic in five words or less.'
+  const response = await openai.createCompletion({
+    'model': 'text-davinci-003',
+    'prompt': 'Sound enthusiastic in five words or less.'
+  })
+  movieBossText.innerText = response.data.choices[0].text.trim()
+  setupInputContainer.innerHTML = `<textarea id="setup-textarea" placeholder="Enter your prompt here..."></textarea>`
+}
+
+// async function fetchBotReply() {
+//   const response = await openai.createCompletion({
+//     'model': 'text-davinci-003',
+//     'prompt': 'Sound enthusiastic in five words or less.'
+//   })
+//   movieBossText.innerText = response.data.choices[0].text.trim()
+// }
+
+async function postRequest() {
+  const response = await fetch('https://line-gpt-app-ogcot.ondigitalocean.app/chat?username=hazdik', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "message": "give me chinese food recommendation"
+    })
+  });
+
+  const data = await response.json();
+  console.log(data);
+}
+
+postRequest();
