7beb7dd5d2ec916cc02e7d928cd3a0a3aa464ba8
Quang Huy <pqhuy.iac@gmail.com>
2023-12-01 22:08:18 +0800

    import the ChatComponent

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { process } from '/env'
 import { Configuration, OpenAIApi } from 'openai'
 
@@ -16,14 +18,21 @@ document.getElementById("send-btn").addEventListener("click", async () => {
   setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
   movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
   // }
-  await fetchBotReply()
+  // await fetchBotReply()
 })
+
+//Render the chatbotComponent
+ReactDOM.render(
+  <ChatComponent />,
+  document.getElementById('root')
+);
 
 async function fetchBotReply() {
   const promptText = setupTextarea.value || 'Sound enthusiastic in five words or less.'
   const response = await openai.createCompletion({
     'model': 'text-davinci-003',
     'prompt': 'Sound enthusiastic in five words or less.'
+
   })
   movieBossText.innerText = response.data.choices[0].text.trim()
   setupInputContainer.innerHTML = `<textarea id="setup-textarea" placeholder="Enter your prompt here..."></textarea>`
@@ -37,19 +46,19 @@ async function fetchBotReply() {
 //   movieBossText.innerText = response.data.choices[0].text.trim()
 // }
 
-async function postRequest() {
-  const response = await fetch('https://line-gpt-app-ogcot.ondigitalocean.app/chat?username=hazdik', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      "message": "give me chinese food recommendation"
-    })
-  });
+// async function postRequest() {
+//   const response = await fetch('https://line-gpt-app-ogcot.ondigitalocean.app/chat?username=hazdik', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//       "message": "give me chinese food recommendation"
+//     })
+//   });
 
-  const data = await response.json();
-  console.log(data);
-}
+//   const data = await response.json();
+//   console.log(data);
+// }
 
 postRequest();
